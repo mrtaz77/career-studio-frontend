cdd73943bde34e5e2f9428102ef6c60cef5cdaff
Sheikh Rahat Mahmud <98137553+Rahat463@users.noreply.github.com>
2025-05-23 21:25:13 +0600

    fix: some conflicts regarding eslint

--- eslint.config.js
+++ eslint.config.js
@@ -54,7 +54,17 @@ export default defineConfig([
       ...pluginReact.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       ...prettierConfig.rules,
-
+      'no-unused-vars': 'off',
+      // …and configure the TS-aware one instead
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_', // ignore unused args that start with _
+          varsIgnorePattern: '^_', // likewise for variables
+          ignoreRestSiblings: true, // for object rest spreads
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'prettier/prettier': 'warn',
       '@typescript-eslint/no-unused-vars': ['warn'],
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,8 @@
-
 // import { initializeApp } from "firebase/app";
- import { getAuth } from "firebase/auth";
+import { getAuth } from 'firebase/auth';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
@@ -13,11 +12,11 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId:import.meta.env.VITE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 //export default app;
- export const auth = getAuth(app);
+export const auth = getAuth(app);
