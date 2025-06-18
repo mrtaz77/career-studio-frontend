4c7f906c9cfe108ab6894beed579b3a589183122
mrtaz77 <mrtazwar23@gmail.com>
2025-06-18 14:36:58 +0600

    feat: update react-day-picker to replace lovable-tagger with vite-plugin-inspect

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import { componentTagger } from 'lovable-tagger';
+import Inspect from 'vite-plugin-inspect';
 
 export default defineConfig(({ mode }) => ({
   server: {
     host: '0.0.0.0', // <-- bind to all IPv4/IPv6
     port: 8080,
   },
-  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === 'development' && Inspect()].filter(Boolean),
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
