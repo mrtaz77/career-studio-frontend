e8f7b19a14321853e7a8c08b028f0b2dfdd1a06b
Sheikh Rahat Mahmud <98137553+Rahat463@users.noreply.github.com>
2025-07-01 02:52:01 +0600

    chore(test): initialize testing framework

---
+++ babel.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    // Transform import.meta to a global variable
+    function () {
+      return {
+        visitor: {
+          MetaProperty(path) {
+            if (path.node.meta.name === 'import' && path.node.property.name === 'meta') {
+              path.replaceWithSourceString('global.import.meta');
+            }
+          },
+        },
+      };
+    },
+  ],
+};
